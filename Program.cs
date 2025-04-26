2011efd4cecf492dcc60d18f5102a0d9e66c11b6
RodrigoMartinez <rodrigomartinezdev@outlook.com>
2025-04-26 17:59:46 -0600

    Creacion del proyecto e instalacion de la libreria Spectre

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number.");
